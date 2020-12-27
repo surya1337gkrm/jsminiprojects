6f6e7f2308188b450b68cc5faa22f6c8187dca4e
Surya Venkatesh Vijjana <surya.venkatesh@tredence.com>
2020-12-27 12:21:13 +0530

    Rotating Nav

--- RotatingNav/script.js
+++ RotatingNav/script.js
@@ -2,6 +2,18 @@ const open = document.querySelector('#open');
 const close = document.querySelector('#close');
 const container = document.querySelector('.container');
 
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
 open.addEventListener('click', () => container.classList.add('show-nav'));
 
 close.addEventListener('click', () => container.classList.remove('show-nav'));
+
+//search
+
+btn.addEventListener('click', () => {
+  search.classList.add('active');
+  input.focus();
+});
+
